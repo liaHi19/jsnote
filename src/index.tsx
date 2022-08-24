13cf601d610984a5a36be387a54280a81c5ae170
liaHi19 <gn.natalia19@gmail.com>
2022-08-24 08:10:51 +0300

    [#3] Add modification of a fetch-plugin: posibility to work with css files

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
@@ -35,7 +35,6 @@ const App = () => {
         global: "window",
       },
     });
-    console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
