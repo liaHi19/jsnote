d8d417d08101731c000fd1d9d49407d35e68fc6e
liaHi19 <gn.natalia19@gmail.com>
2022-08-24 11:37:41 +0300

    [#4] Add iframe for preview the code

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
-  const [code, setCode] = useState("");
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -25,6 +25,7 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -35,16 +36,41 @@ const App = () => {
         global: "window",
       },
     });
-
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+  <head></head>
+  <body>
+  <div id="root"></div>
+  <script>
+  window.addEventListener("message", (event) => {
+    try{
+      eval(event.data)
+    } catch(err){
+      const root = document.querySelector("#root");
+      root.innerHTML = "<div style='color: red;'> <h4>Runtime Error</h4>" + err + "</div>"
+      throw err;
+    }
+
+  }, false)
+  </script>
+  </body>
+  </html>
+  `;
   return (
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
