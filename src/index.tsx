184b1726ab7a94a70541007fa5936a05a97a9e4b
liaHi19 <gn.natalia19@gmail.com>
2022-08-27 10:09:19 +0300

    [#7] Add Preview component

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,11 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/codeEditor/CodeEditor";
 
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import Preview from "./components/preview/Preview";
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -29,7 +30,6 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    iframe.current.srcdoc = html;
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -40,29 +40,9 @@ const App = () => {
         global: "window",
       },
     });
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
 
-  const html = `
-  <html>
-  <head></head>
-  <body>
-  <div id="root"></div>
-  <script>
-  window.addEventListener("message", (event) => {
-    try{
-      eval(event.data)
-    } catch(err){
-      const root = document.querySelector("#root");
-      root.innerHTML = "<div style='color: red;'> <h4>Runtime Error</h4>" + err + "</div>"
-      throw err;
-    }
-
-  }, false)
-  </script>
-  </body>
-  </html>
-  `;
   return (
     <div>
       <CodeEditor
@@ -71,16 +51,10 @@ const App = () => {
           setInput(value);
         }}
       />
-      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
