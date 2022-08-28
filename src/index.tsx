49a2a2d57e229551a13aa129e8452bf85424d717
liaHi19 <gn.natalia19@gmail.com>
2022-08-28 08:07:45 +0300

    [#8] Add bundle function: dividing functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,20 @@
-import * as esbuild from "esbuild-wasm";
-import React, { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { bundle } from "./components/bundler";
 
 import CodeEditor from "./components/codeEditor/CodeEditor";
-
-import "bulmaswatch/superhero/bulmaswatch.min.css";
 import Preview from "./components/preview/Preview";
 
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+
 const App = () => {
-  const ref = useRef<any>();
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
