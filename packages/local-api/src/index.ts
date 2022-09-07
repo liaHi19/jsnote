48d44c27bccaab42ca9282b0ea6a78e23157868d
liaHi19 <gn.natalia19@gmail.com>
2022-09-07 18:43:12 +0300

    [#24] Add set up of the serveCommand

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log("Server is listening");
+export const serve = (filename: string, port: number, dir: string) => {
+  console.log("serving traffic on port", port);
+  console.log("saving/fetching cells from", filename);
+  console.log("That file is in dir", dir);
 };
